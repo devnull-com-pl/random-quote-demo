b2f6bfd93a435def4f0e6e880ef2cba4152cd447
grenn <krzysztof.pogorzelski@devmull.com.pl>
2021-08-19 14:36:32 +0200

    fix

--- app/index.php
+++ app/index.php
@@ -2,42 +2,45 @@
   // Code based on https://php.happycodings.com/html-and-php/code19.html
 
   // DB connection info
-  $DBUSER = "simple-quote";
-  $DBPASS = "RED!hat@";
-  $DBHOST = "localhost";
-  $DBNAME = "quotes";
+  $DBUSER = getenv("MYSQL_USER");
+  $DBPASS = getenv("MYSQL_PASSWORD");
+  $DBHOST = getenv("MYSQL_SERVICE_HOST");
+  $DBNAME = getenv("MYSQL_DATABASE");
      
   // initialize variable that will hold the quote. 
   $quote = ""; 
   // establish connection to the database containing the quotes. 
-  $db = mysql_connect($DBHOST, $DBUSER, $DBPASS) or die ("Unable to connect to database."); 
-  mysql_select_db($DBNAME) or die ("Unable to select database."); 
+  $db = new mysqli($DBHOST, $DBUSER, $DBPASS, $DBNAME) //or die ("Unable to connect to database."); 
+  if ($db->connect_error) {
+    die("Connection failed. Please make sure you have the MYSQL_SERVICE_HOST, MYSQL_USER, MYSQL_PASSWORD, and MYSQL_DATABASE environment variables : " . $db->connect_error);
+  }
+  //mysql_select_db($DBNAME) or die ("Unable to select database."); 
   
   // select the quotes that have not been displayed (q_mark = 0). 
   $sql = "SELECT * from quote WHERE q_mark = 0"; 
-  $result = mysql_query($sql); 
+  $result = mysqli_query($sql); 
   // simple error checking 
-  if (mysql_errno()>0) { 
-    echo "<BR>\n<FONT COLOR=\"#990000\">".mysql_errno().": ".mysql_error()."<BR>\n"; 
+  if (mysqli_errno()>0) { 
+    echo "<BR>\n<FONT COLOR=\"#990000\">".mysqli_errno().": ".mysqli_error()."<BR>\n"; 
     exit; 
   } 
   // put the number of rows found into $max. 
-  $max = mysql_num_rows($result)-1; 
+  $max = mysqli_num_rows($result)-1; 
   
   // if we do not find an available quote, then mark them (q_mark) to 0 and select again. 
   if ($max < 0) { 
-    $result = mysql_query("UPDATE quote SET q_mark = 0"); 
-      if (mysql_errno()>0) { 
-        echo "<BR>\n<FONT COLOR=\"#990000\">".mysql_errno().": ".mysql_error()."<BR>\n"; 
+    $result = mysqli_query("UPDATE quote SET q_mark = 0"); 
+      if (mysqli_errno()>0) { 
+        echo "<BR>\n<FONT COLOR=\"#990000\">".mysqli_errno().": ".mysqli_error()."<BR>\n"; 
         exit; 
       } 
     $sql = "SELECT * from quote WHERE q_mark = 0"; 
-    $result = mysql_query($sql); 
-    if (mysql_errno()>0) { 
-      echo "<BR>\n<FONT COLOR=\"#990000\">".mysql_errno().": ".mysql_error()."<BR>\n"; 
+    $result = mysqli_query($sql); 
+    if (mysqli_errno()>0) { 
+      echo "<BR>\n<FONT COLOR=\"#990000\">".mysqli_errno().": ".mysqli_error()."<BR>\n"; 
       exit; 
     } 
-    $max = mysql_num_rows($result)-1; 
+    $max = mysqli_num_rows($result)-1; 
   } 
   // generate a random number between 0 and the number of quotes available. 
   mt_srand((double)microtime()*1000000); 
@@ -48,14 +51,14 @@
   } 
   // select the random quote and store the text in $quote and it's id in $id. 
   for ($x=0;$x<=$random;$x++) { 
-    $myrow = mysql_fetch_array($result); 
+    $myrow = mysqli_fetch_array($result); 
   } 
   $id = $myrow[0]; 
   $quote = $myrow[1]; 
   // mark this selected quote as displayed (q_mark = 1). 
-  $result = mysql_query("UPDATE quote SET q_mark = 1 WHERE q_id = '$id'"); 
-  if (mysql_errno()>0) { 
-    echo "<BR>\n<FONT COLOR=\"#990000\">".mysql_errno().": ".mysql_error()."<BR>\n"; 
+  $result = mysqli_query("UPDATE quote SET q_mark = 1 WHERE q_id = '$id'"); 
+  if (mysqli_errno()>0) { 
+    echo "<BR>\n<FONT COLOR=\"#990000\">".mysqli_errno().": ".mysqli_error()."<BR>\n"; 
     exit; 
   } 
   // convert to HTML special characters, you know, like ?, ?, ?, and so on. 
